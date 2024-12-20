c16aeccd841e551c848396a8971a9d7ec5ffeb8f
MichalKowalski0 <147046048+MichalKowalski0@users.noreply.github.com>
2024-12-20 14:33:49 +0100

    Add files via upload

---
+++ nr_albumu174693/174693_kolokwium_src/Renifer.java
@@ -0,0 +1,9 @@
+public class Renifer {
+    private String imie;
+    private int predkosc;
+
+    public Renifer(String imie, int predkosc) {
+        this.imie = imie;
+        setPredkosc(predkosc);
+    }
+}
