dbdd873828d0688ae6bb59190232f6aef62fa40f
MichalKowalski0 <147046048+MichalKowalski0@users.noreply.github.com>
2024-12-20 14:34:18 +0100

    Delete nr_albumu174693 directory

--- nr_albumu174693/174693_kolokwium_src/Renifer.java
+++
@@ -1,9 +0,0 @@
-public class Renifer {
-    private String imie;
-    private int predkosc;
-
-    public Renifer(String imie, int predkosc) {
-        this.imie = imie;
-        setPredkosc(predkosc);
-    }
-}
